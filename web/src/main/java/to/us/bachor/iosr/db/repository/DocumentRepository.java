1bc7de74d6020b4aeb91621e69e743a4454c3979
greenjoe <joesqsek@gmail.com>
2014-05-17 23:59:55 +0200

    Moving common classes and settings files to a new "core" maven module

--- web/src/main/java/to/us/bachor/iosr/db/repository/DocumentRepository.java
+++
@@ -1,9 +0,0 @@
-package to.us.bachor.iosr.db.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import to.us.bachor.iosr.db.model.Document;
-
-public interface DocumentRepository extends MongoRepository<Document, String> {
-
-}
